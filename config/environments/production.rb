fd02c3a38e6d6d9a164b6b4854ea1411e4809599
城戸　真祐 <downmap555@gmail.com>
2018-01-23 17:01:29 +0900

    Fix mailer definition

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV['LOGIN_NAME'],
+    password: ENV['LOGIN_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,15 +1,14 @@
-if Rails.env.production?
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.gmail.com',
-    domain: 'gmail.com',
-    port: 587,
-    user_name: ENV['LOGIN_NAME'],
-    password: ENV['LOGIN_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
-elsif Rails.env.development?
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.gmail.com',
+  domain: 'gmail.com',
+  port: 587,
+  user_name: ENV['LOGIN_NAME'],
+  password: ENV['LOGIN_PASSWORD'],
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
+if Rails.env.development?
   ActionMailer::Base.delivery_method = :letter_opener
 else
   ActionMailer::Base.delivery_method = :test
